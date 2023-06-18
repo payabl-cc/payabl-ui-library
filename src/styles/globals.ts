14d3b56962519c88c1a0ed8d10d1d8ca69d82541
Rodrigo Veiga <rodrigoveiga1206@gmail.com>
2023-06-18 15:04:29 -0300

    publish to npm

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
+// For use with Storybook
 export const GlobalStyles = createGlobalStyle`
   @font-face {
     font-family: "Work Sans Light";
